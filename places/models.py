7c8ee7bb3283f6ea18021545af39078015bef209
Olga <o.panova27@gmail.com>
2023-01-12 22:48:45 +0300

    Setup media files serving

--- places/models.py
+++ places/models.py
@@ -2,27 +2,27 @@ from django.db import models
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
     
-    description_short = models.TextField(verbose_name="Краткое описание")
-    description_long = models.TextField(verbose_name="Полное описание")
+    description_short = models.TextField(verbose_name='Краткое описание')
+    description_long = models.TextField(verbose_name='Полное описание')
     
-    coordinate_lng = models.FloatField(verbose_name="Долгота")
-    coordinate_lat = models.FloatField(verbose_name="Широта")
+    coordinate_lng = models.FloatField(verbose_name='Долгота')
+    coordinate_lat = models.FloatField(verbose_name='Широта')
     
     def __str__(self):
         return self.title
 
 
 class PlaceImage(models.Model):
-    image = models.ImageField(verbose_name="Изображение")
+    image = models.ImageField(verbose_name='Изображение')
     
-    sort = models.IntegerField(verbose_name="Сортировка")
+    sort = models.IntegerField(verbose_name='Сортировка')
     
     place = models.ForeignKey(
         Place,
         on_delete=models.CASCADE,
-        verbose_name="Место",
+        verbose_name='Место',
     )
     
     def __str__(self):
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
@@ -7,4 +9,4 @@ from where_to_go import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.start_page),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
