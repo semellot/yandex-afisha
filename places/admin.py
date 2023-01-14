e94e9e9d6c1dd23a96f12c72af4bab0e95fc004c
Olga <o.panova27@gmail.com>
2023-01-15 00:36:37 +0300

    Add preview images in admin

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    readonly_fields = ['headshot_image']
+    
+    def headshot_image(self, obj):
+        return format_html('<img src="{url}" height="200px" />'.format(
+            url = obj.image.url)
+    )
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
