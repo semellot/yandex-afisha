6ba95dbd459af38865f450e2928554e347070332
Olga <o.panova27@gmail.com>
2023-01-15 00:11:04 +0300

    Unload JSON

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,18 +1,20 @@
+import json
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
-from places.models import Place
+from places.models import Place, PlaceImage
 
 
 def start_page(request):
-    json = {
+    geo_json = {
         'type': 'FeatureCollection',
         'features': []
     }
     places = Place.objects.all()
     
     for place in places:
-        json['features'].append(
+        geo_json['features'].append(
             {
                 'type': 'Feature',
                 'geometry': {
@@ -27,9 +29,25 @@ def start_page(request):
             }
         )
     
-    return render(request, 'index.html', {'json': json})
+    return render(request, 'index.html', {'json': geo_json})
 
 
 def detail_place(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
-    return render(request, 'detail_place.html', {'place': place})
+    response_data = {
+        'title': place.title,
+        'imgs': [],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates': {
+            'lng': place.coordinate_lng,
+            'lat': place.coordinate_lat
+        }
+    }
+    
+    place_images = PlaceImage.objects.filter(place=place)
+    for place_image in place_images:
+        response_data['imgs'].append(place_image.image.url)
+    
+    return HttpResponse(json.dumps(response_data, ensure_ascii=False),
+         content_type='application/json')
