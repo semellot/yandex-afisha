9bf2d7611d7bc1a0a9fb2f07fa956f8c9fd13b79
Olga <o.panova27@gmail.com>
2023-01-26 19:15:20 +0300

    Change defaule value for DEBUG

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
